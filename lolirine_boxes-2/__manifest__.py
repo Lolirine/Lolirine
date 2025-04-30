147413b2d423345f882cccff70e2c9149258d3fa
Lolirine <gardemeublelolirine@gmail.com>
2025-04-30 16:31:35 +0200

    Add files via upload

---
+++ lolirine_boxes-2/__manifest__.py
@@ -0,0 +1,20 @@
+{
+  "name": "Lolirine Boxes Visualisation",
+  "version": "1.0",
+  "author": "Erin",
+  "category": "Website",
+  "depends": ["website"],
+  "data": [
+    "views/boxes_template.xml",
+    "views/boxes_menu.xml"
+  ],
+  "assets": {
+    "web.assets_frontend": [
+      "lolirine_boxes/static/src/css/boxes.css",
+      "lolirine_boxes/static/src/js/boxes.js"
+    ]
+  },
+  "installable": True,
+  "application": True,
+  "auto_install": False
+}
