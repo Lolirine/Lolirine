254d230445baa69611fc45f261da9c7bf9a78240
Lolirine <gardemeublelolirine@gmail.com>
2025-05-03 20:11:15 +0200

    Delete box_storage_map directory

--- box_storage_map/controllers/main.py
+++
@@ -1,9 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-class BoxMapController(http.Controller):
-
-    @http.route('/boxes/plan', type='http', auth='public', website=True)
-    def box_map(self, **kwargs):
-        boxes = request.env['box.stockage'].sudo().search([])
-        return request.render('box_storage_map.box_map_template', {'boxes': boxes})
