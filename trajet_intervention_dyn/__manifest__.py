397b4e183b223fc4355854f7b096ebae553ccf0a
Lolirine <gardemeublelolirine@gmail.com>
2025-05-03 16:25:37 +0200

    Add files via upload

---
+++ trajet_intervention_dyn/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Trajets Interventions (Dynamique)",
+    "version": "1.0",
+    "category": "Garde-Meubles",
+    "summary": "Trajets avec vues générées dynamiquement (pas de XML)",
+    "author": "Erin",
+    "depends": ["base"],
+    "installable": true,
+    "application": false
+}
