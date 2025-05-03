a4e27b9168b6013101d7a671b935a3159e01ba45
Lolirine <gardemeublelolirine@gmail.com>
2025-05-03 14:51:29 +0200

    Add files via upload

---
+++ test_assets/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Test Assets',
+    'version': '1.0',
+    'summary': 'Module de test pour vérifier les assets',
+    'category': 'Tools',
+    'author': 'Erin',
+    'depends': ['web'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
