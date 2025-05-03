3d73ff32c7bc465f0986398719de573e6518247f
Lolirine <gardemeublelolirine@gmail.com>
2025-05-03 20:11:59 +0200

    Delete trajet_intervention_min directory

--- trajet_intervention_min/models/trajet_intervention.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class TrajetIntervention(models.Model):
-    _name = 'trajet.intervention'
-    _description = 'Test Intervention Minimal'
-
-    name = fields.Char(string='Nom du trajet')
