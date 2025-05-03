5970f13b1ee6952db2a33d9c6a8d3029fa772d0a
Lolirine <gardemeublelolirine@gmail.com>
2025-05-03 20:16:51 +0200

    Delete box_storage_map 2 directory

--- box_storage_map 2/models/map.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class BoxStorageMap(models.Model):
-    _name = 'box.storage.map'
-    _description = 'Carte de stockage des boxes'
-
-    name = fields.Char("Nom")
-    position_x = fields.Integer("Position X")
-    position_y = fields.Integer("Position Y")
