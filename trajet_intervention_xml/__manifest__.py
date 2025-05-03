196e11b73d42806a15e3071cb4bd13a2d053a2c6
Lolirine <gardemeublelolirine@gmail.com>
2025-05-03 17:14:17 +0200

    Add files via upload

---
+++ trajet_intervention_xml/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Interventions - Trajets",
+    "version": "1.0",
+    "category": "Operations",
+    "summary": "Gère les trajets d'intervention pour garde-meubles",
+    "author": "Erin",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/trajet_intervention_views.xml",
+        "views/menu.xml"
+    ],
+    "installable": True,
+    "application": True
+}
