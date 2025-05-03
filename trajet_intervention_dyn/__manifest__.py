9bfb01714f160a81ae9c85fc9429bca8d590ee78
Lolirine <gardemeublelolirine@gmail.com>
2025-05-03 17:03:55 +0200

    Add files via upload

---
+++ trajet_intervention_dyn/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Interventions",
+    "version": "1.0",
+    "category": "Garde-Meubles",
+    "summary": "App complète pour gérer les trajets d'intervention",
+    "author": "Erin",
+    "depends": ["base"],
+    "installable": true,
+    "application": true
+}
