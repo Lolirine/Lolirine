330b8d1c65d857f8bee6a8c4337d12a7f1fa00d9
Lolirine <gardemeublelolirine@gmail.com>
2025-05-06 18:03:25 +0200

    Delete penalite_client_final directory

--- penalite_client_final/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-
-from odoo import models, fields
-
-class ResPartner(models.Model):
-    _inherit = 'res_partner'
-
-    penalite_ids = fields.One2many('penalite_client', 'partner_id', string="Pénalités")
