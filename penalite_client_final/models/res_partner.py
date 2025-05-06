88081d8f43f98765ff3e9f8407be142f8d8e4f26
Lolirine <gardemeublelolirine@gmail.com>
2025-05-06 17:37:28 +0200

    Update res_partner.py

--- penalite_client_final/models/res_partner.py
+++ penalite_client_final/models/res_partner.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    penalite_ids = fields.One2many('penalite.client', 'partner_id', string="Pénalités")
+    penalite_ids = fields.One2many('penalite_client', 'partner_id', string="Pénalités")
