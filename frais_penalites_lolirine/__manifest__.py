51b5f2c6724d93b3dd972463e4e9b3700ec809b8
Lolirine <gardemeublelolirine@gmail.com>
2025-05-06 11:08:49 +0200

    Add files via upload

---
+++ frais_penalites_lolirine/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Frais et Pénalités',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Gestion des frais et pénalités liés aux abonnements',
+    'depends': ['base', 'sale_subscription', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/penalite_views.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
