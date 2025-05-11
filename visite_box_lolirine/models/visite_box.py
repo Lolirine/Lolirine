711f069f43641f5a4fc53cfc62f1309ea3575771
Lolirine <gardemeublelolirine@gmail.com>
2025-05-11 10:12:19 +0200

    Add files via upload

---
+++ visite_box_lolirine/models/visite_box.py
@@ -0,0 +1,22 @@
+from odoo import models, fields
+
+class VisiteBox(models.Model):
+    _name = 'visite.box'
+    _description = 'Visite de box garde-meubles'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    name = fields.Char("Nom du client", required=True)
+    email = fields.Char("Email")
+    phone = fields.Char("Téléphone")
+    date_visite = fields.Datetime("Date de la visite", required=True)
+    type_box = fields.Selection([
+        ('petit', 'Petit'),
+        ('moyen', 'Moyen'),
+        ('grand', 'Grand')
+    ], string="Taille souhaitée", required=True)
+    state = fields.Selection([
+        ('draft', 'À confirmer'),
+        ('confirmed', 'Confirmée'),
+        ('done', 'Réalisée')
+    ], default='draft', string="Statut", tracking=True)
+    signature = fields.Binary("Signature du client")
