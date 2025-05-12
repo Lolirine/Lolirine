7fd9610d7090a9f82e7b073cba55be1f3b8c9c7d
Lolirine <gardemeublelolirine@gmail.com>
2025-05-12 16:57:05 +0200

    Add files via upload

---
+++ lolirine_reservation_snippet/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Lolirine - Réservation Visite",
+    "version": "1.0",
+    "summary": "Snippet personnalisé pour la réservation de visite",
+    "category": "Website",
+    "author": "Srl Lolirine",
+    "license": "OPL-1",
+    "depends": ["website"],
+    "data": [
+        "views/snippet_template.xml"
+    ],
+    "installable": True,
+    "application": False
+}
