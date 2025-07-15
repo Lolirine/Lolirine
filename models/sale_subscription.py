f81f124b9680759a20667dd09db76b21c0a802fa
Lolirine <gardemeublelolirine@gmail.com>
2025-07-15 18:25:22 +0200

    Delete models directory

--- models/sale_subscription.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class SaleSubscription(models.Model):
-    _inherit = 'sale.subscription'
-
-    # Vous pouvez ajouter des champs spécifiques au contrat de bail ici si nécessaire
-    # Par exemple :
-    # numero_espace_stockage = fields.Char(string="Numéro de l'espace de stockage")
