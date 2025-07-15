3c67cec19659629a55a7ee6f950ef388777ffac3
Lolirine <gardemeublelolirine@gmail.com>
2025-07-15 18:29:23 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,19 +1,16 @@
-# -*- coding: utf-8 -*-
-{
-    'name': "Rapport de Contrat de Bail sur Abonnement",
-    'summary': "Ajoute un rapport de contrat de bail pour les abonnements d'espaces de stockage.",
-    'description': "Ce module intègre un rapport de contrat de bail au module Abonnement d'Odoo.",
-    'author': "Votre Nom",
-    'website': "https://www.votre-site.com",
-    'category': 'Sales/Subscription',
-    'version': '1.0',
-    'depends': ['sale_subscription'],
-    'data': [
-        'report/report_contrat_bail.xml',
-        'report/template_contrat_bail.xml',
-        'views/sale_subscription_views.xml',
-    ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
-}
+
+**2. `report/report_contrat_bail.xml`**
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<odoo>
+    <record id="action_report_contrat_bail" model="ir.actions.report">
+        <field name="name">Contrat de Bail</field>
+        <field name="model">sale.subscription</field>
+        <field name="report_type">qweb-pdf</field>
+        <field name="report_name">contrat_bail_abonnement.report_contrat_bail_template</field>
+        <field name="report_file">contrat_bail_abonnement.report_contrat_bail_template</field>
+        <field name="print_report_name">'Contrat - %s' % (object.name)</field>
+        <field name="binding_model_id" ref="sale_subscription.model_sale_subscription"/>
+        <field name="binding_type">report</field>
+    </record>
+</odoo>
