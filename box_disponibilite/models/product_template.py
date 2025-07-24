e72d82043b3370fcf615657b7648542b3768e23a
Lolirine <gardemeublelolirine@gmail.com>
2025-07-24 17:13:19 +0200

    Delete box_disponibilite directory

--- box_disponibilite/models/product_template.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    disponible_a_la_location = fields.Boolean(string="Disponible à la location", default=True)
