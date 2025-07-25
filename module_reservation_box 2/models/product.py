aef6a9f377ba5743fe1215297b6d37cf8778263d
Lolirine <gardemeublelolirine@gmail.com>
2025-07-25 15:06:31 +0200

    Delete module_reservation_box 2 directory

--- module_reservation_box 2/models/product.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    x_disponible = fields.Boolean(string='Disponible pour réservation', default=False)
