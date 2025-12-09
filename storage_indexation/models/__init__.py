389efc4b9b44a89d1d0cfdba84b46a6bd4552c2c
Lolirine <gardemeublelolirine@gmail.com>
2025-12-09 19:11:04 +0100

    Delete storage_indexation directory

--- storage_indexation/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import storage_price_index
-from . import storage_indexation
-from . import sale_subscription
-from . import res_config_settings
--- storage_indexation/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import storage_indexation_wizard
