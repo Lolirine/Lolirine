585b0e4740b3f0fa74797d786b57ee1b21e1b04e
Lolirine <gardemeublelolirine@gmail.com>
2025-12-19 17:13:28 +0100

    Add files via upload

---
+++ km_expense/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from . import km_bareme
+from . import km_vehicule
+from . import km_destination
+from . import km_trajet
+from . import km_expense
+from . import hr_expense
+from . import res_config_settings
+from . import account_move
+from . import res_partner
