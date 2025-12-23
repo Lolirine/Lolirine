2f177b7a7a8287f78572e82667a32222c8d505db
Lolirine <gardemeublelolirine@gmail.com>
2025-12-23 16:42:56 +0100

    Add files via upload

---
+++ act365_integration/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import act365_api
+from . import act365_cardholder_group
+from . import res_config_settings
+from . import sale_subscription
+from . import res_partner
