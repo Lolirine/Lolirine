a1cb4a6305a34756044d2fae4d2523534f795808
Lolirine <gardemeublelolirine@gmail.com>
2025-12-23 15:43:22 +0100

    Add files via upload

---
+++ storage_plan_module/models/storage_floor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class StorageFloor(models.Model):
+    _name = 'storage.floor'
+    _description = 'Étage du garde-meubles'
+    _order = 'sequence, name'
+
+    name = fields.Char(string='Nom', required=True)
+    sequence = fields.Integer(string='Séquence', default=10)
+    code = fields.Char(string='Code', required=True)
+    active = fields.Boolean(string='Actif', default=True)
+    
+    # Noms des allées
+    aisle_left_name = fields.Char(string='Nom allée gauche', default='Allée gauche')
+    aisle_right_name = fields.Char(string='Nom allée droite', default='Allée droite')
+    
+    box_ids = fields.One2many('storage.box', 'floor_id', string='Boxes')
+    box_count = fields.Integer(string='Nombre de boxes', compute='_compute_box_count')
+    available_box_count = fields.Integer(string='Boxes disponibles', compute='_compute_box_count')
+
+    @api.depends('box_ids', 'box_ids.status')
+    def _compute_box_count(self):
+        for floor in self:
+            floor.box_count = len(floor.box_ids)
+            floor.available_box_count = len(floor.box_ids.filtered(lambda b: b.status == 'disponible'))
