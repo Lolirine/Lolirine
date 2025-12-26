ecd843e8951cdd1ddbb01f908bd974a1ccfaa2b7
Lolirine <gardemeublelolirine@gmail.com>
2025-12-26 16:28:04 +0100

    Add files via upload

---
+++ apartment_rental/models/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from . import apartment_property
+from . import apartment_tenant
+from . import apartment_lease
+from . import apartment_inventory
+from . import apartment_inventory_line
+from . import apartment_inventory_photo
+from . import apartment_control_visit
+from . import apartment_rent
+from . import apartment_meter
+from . import apartment_meter_reading
+from . import apartment_intervention
+from . import apartment_document
+from . import apartment_index_history
+from . import apartment_room_type
+from . import apartment_maintenance_contract
