b8bcb5d5e0776b6acfe0491fe999c41cc0b0051c
Lolirine <gardemeublelolirine@gmail.com>
2026-01-07 16:47:50 +0100

    Update account_move.py

--- km_expense/models/account_move.py
+++ km_expense/models/account_move.py
@@ -237,16 +237,15 @@ class AccountMove(models.Model):
         }
 
     def action_view_km_trajets(self):
-        """Voir les trajets liés à cette facture"""
-        self.ensure_one()
-        return {
-            'name': 'Trajets',
-            'type': 'ir.actions.act_window',
-            'res_model': 'km.trajet',
-            'view_mode': 'tree,form',
-            'domain': [('invoice_id', '=', self.id)],
-            'context': {'default_invoice_id': self.id},
-        }
+    """Voir les trajets liés à cette facture"""
+    self.ensure_one()
+    return {
+        'name': 'Trajets',
+        'type': 'ir.actions.act_window',
+        'res_model': 'km.trajet',
+        'view_mode': 'list,form',  # ← Nouveau nom Odoo 19
+        ...
+    }
 
     def action_create_km_trajet_auto(self):
         """Créer automatiquement un trajet pour cette facture (même brouillon)"""
