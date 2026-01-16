5f6bdd035ed51d0f4ad6e80ed921be96cfb543fd
Lolirine <gardemeublelolirine@gmail.com>
2026-01-16 19:04:52 +0100

    Add files via upload

---
+++ lolirine_pool_website/__manifest__.py
@@ -0,0 +1,52 @@
+{
+    'name': 'Lolirine Pool Store - Website',
+    'version': '19.0.1.0.0',
+    'category': 'Website/Website',
+    'summary': 'Site e-commerce Lolirine Pool Store - Matériel de piscine',
+    'description': """
+        Site e-commerce pour la vente de matériel de piscine
+        =====================================================
+        
+        Ce module crée et configure le site web Lolirine Pool Store :
+        
+        - Création du 2ème site web (multi-site)
+        - Domaine : lolirine-pool.be
+        - Thème personnalisé bleu/piscine
+        - Pages : Accueil, Catégories, À propos, Contact
+        - Filtrage des produits par site web
+        - E-commerce intégré
+        - Clients partagés avec le site principal
+        
+        Compatible avec lolirine_pool_import pour l'import des produits.
+    """,
+    'author': 'Lolirine SPRL',
+    'website': 'https://lolirine-pool.be',
+    'license': 'LGPL-3',
+    'depends': [
+        'website',
+        'website_sale',
+        'website_sale_wishlist',
+        'website_sale_comparison',
+        'product',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/website_data.xml',
+        'data/product_category_data.xml',
+        'data/website_menu_data.xml',
+        'data/website_page_data.xml',
+        'views/product_template_views.xml',
+        'views/website_templates.xml',
+        'views/snippets/hero_pool.xml',
+        'views/snippets/categories_grid.xml',
+        'views/snippets/features_pool.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'lolirine_pool_website/static/src/css/pool_theme.css',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
