d975a90997a5475efd15d7b428cf15f5edf1d174
Lolirine <gardemeublelolirine@gmail.com>
2026-01-23 15:35:06 +0100

    Delete lolirine_pool_dropship directory

--- lolirine_pool_dropship/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import dropship_config
-from . import supplier_dropship_info
-from . import product_template
-from . import sale_order
-from . import purchase_order
-from . import dropship_decision_log
-from . import res_partner
