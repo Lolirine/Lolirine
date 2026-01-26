4cac1f401c5c2a78169fb58b0d37a9e77e9d574a
Lolirine <gardemeublelolirine@gmail.com>
2026-01-26 15:53:17 +0100

    Update account_move.py

--- lolirine_invoice/models/account_move.py
+++ lolirine_invoice/models/account_move.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 from datetime import date, timedelta
 import logging
 
-_logger = logging.getLogger(__name_)
+_logger = logging.getLogger(__name__)
 
 
 class AccountMove(models.Model):
@@ -679,3 +679,4 @@ class SaleSubscription(models.Model):
                 move.auto_send_peppol = True
         
         return moves
+
