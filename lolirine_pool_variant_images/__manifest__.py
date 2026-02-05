432f6c9641a99bf8329e5721d1989673b45cc2ba
Lolirine <gardemeublelolirine@gmail.com>
2026-02-05 16:27:32 +0100

    Add files via upload

--- lolirine_pool_variant_images/__manifest__.py
+++ lolirine_pool_variant_images/__manifest__.py
@@ -8,9 +8,10 @@
         directement depuis la configuration des variantes sur la fiche produit.
         
         Fonctionnalités:
-        - Bouton "Gérer les visuels" sur chaque ligne d'attribut
+        - Onglet "Visuels des variantes" sur la fiche produit
         - Wizard d'édition des images/couleurs par valeur d'attribut
         - Preview des visuels dans la configuration des variantes
+        - Swatches améliorés sur le site web (couleur et image)
         - Support display_type 'image' et 'color'
     """,
     'author': 'Lolirine SPRL',
@@ -25,11 +26,13 @@
         'wizard/attribute_visual_wizard_views.xml',
         'views/product_template_views.xml',
         'views/product_attribute_views.xml',
-        'views/website_templates.xml',
     ],
     'assets': {
         'web.assets_backend': [
             'lolirine_pool_variant_images/static/src/scss/variant_images.scss',
+        ],
+        'web.assets_frontend': [
+            'lolirine_pool_variant_images/static/src/scss/variant_swatches.scss',
         ],
     },
     'installable': True,
