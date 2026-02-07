484366f3e8341f376c1f8ec112ff2e230171919a
Lolirine <gardemeublelolirine@gmail.com>
2026-02-07 14:04:56 +0100

    Create pool_variant_image.py

---
+++ lolirine_pool_import/models/pool_variant_image.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class ProductTemplateAttributeValueImage(models.Model):
+    _inherit = 'product.template.attribute.value'
+
+    variant_image = fields.Image(
+        string='Image variante',
+        max_width=1920, max_height=1920,
+    )
+    variant_image_128 = fields.Image(
+        string='Miniature',
+        related='variant_image',
+        max_width=128, max_height=128,
+        store=True,
+    )
