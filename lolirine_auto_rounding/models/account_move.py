35c908ac2d779e08e3c1c6ca8a866f9b19139612
Lolirine <gardemeublelolirine@gmail.com>
2026-02-10 11:04:54 +0100

    Update account_move.py

--- lolirine_auto_rounding/models/account_move.py
+++ lolirine_auto_rounding/models/account_move.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, api, fields
 
 
@@ -22,13 +21,12 @@ class AccountMove(models.Model):
     def create(self, vals_list):
         """Applique automatiquement l'arrondi sur les factures clients"""
         for vals in vals_list:
-            # Si c'est une facture client et pas d'arrondi défini
-            move_type = vals.get('move_type', self._context.get('default_move_type', 'entry'))
+            move_type = vals.get('move_type', self.env.context.get('default_move_type', 'entry'))
             if move_type in ('out_invoice', 'out_refund') and not vals.get('invoice_cash_rounding_id'):
                 rounding = self.env['account.cash.rounding'].search([
                     ('name', 'ilike', 'Arrondi Euro')
                 ], limit=1)
                 if rounding:
                     vals['invoice_cash_rounding_id'] = rounding.id
-        
+
         return super().create(vals_list)
