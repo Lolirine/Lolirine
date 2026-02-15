5677bc426ab96e63089e1d796c506367893c2f96
Lolirine <gardemeublelolirine@gmail.com>
2026-02-15 18:59:17 +0100

    Add files via upload

---
+++ lolirine_pool_dropship/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import dropship_config
+from . import supplier_dropship_info
+from . import product_template
+from . import sale_order
+from . import purchase_order
+from . import dropship_decision_log
+from . import res_partner
