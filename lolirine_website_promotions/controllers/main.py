74242a0d1f9c261d14ca3cef8661968a15906614
Lolirine <gardemeublelolirine@gmail.com>
2026-03-03 14:39:29 +0100

    Update pricelist retrieval method in main.py

--- lolirine_website_promotions/controllers/main.py
+++ lolirine_website_promotions/controllers/main.py
@@ -26,7 +26,7 @@ class WebsitePromotions(http.Controller):
         website = request.website
 
         # Get current pricelist
-        pricelist = website.pricelist_id
+        pricelist = request.website._get_current_pricelist()
 
         # Search published products on the current website
         domain = website.sale_product_domain()
