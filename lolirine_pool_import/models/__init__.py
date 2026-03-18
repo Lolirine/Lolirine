c4b940f33039640dc6f7c3a0852ca728b438c105
Lolirine <gardemeublelolirine@gmail.com>
2026-03-18 20:35:59 +0100

    Delete lolirine_pool_import directory

--- lolirine_pool_import/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import pool_supplier
-from . import product_template
-from . import pool_import_log
-from . import pool_catalog
-from . import pool_catalog_extraction
-from . import res_config_settings
-from . import pool_variant_image
-
-from . import pool_catalog_pdf_parser
