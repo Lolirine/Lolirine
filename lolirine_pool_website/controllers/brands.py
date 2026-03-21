aa5466ad3a67a20ff5fca542e3d3440140487a91
Lolirine <gardemeublelolirine@gmail.com>
2026-03-21 16:38:55 +0100

    Update brands.py

--- lolirine_pool_website/controllers/brands.py
+++ lolirine_pool_website/controllers/brands.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class BrandsController(http.Controller):
 
     @http.route('/marques', type='http', auth='public', website=True)
     def brands_page(self, **kwargs):
-        """Page de navigation par marque."""
         brands = request.env['pool.brand'].sudo().search([
             ('active', '=', True),
         ], order='sequence, name')
@@ -25,10 +25,42 @@ class BrandsController(http.Controller):
                     'name': brand.name,
                     'logo': brand.logo,
                     'description': brand.description or '',
-                    'website_url': brand.website_url or '',
                     'product_count': count,
                 })
 
         return request.render('lolirine_pool_website.brands_page', {
             'brands': brands_data,
+        })
+
+
+class WebsiteSaleBrands(WebsiteSale):
+
+    @http.route('/shop/marque/<int:brand_id>', type='http', auth='public', website=True)
+    def shop_by_brand(self, brand_id, page=0, **kwargs):
+        brand = request.env['pool.brand'].sudo().browse(brand_id)
+        if not brand.exists():
+            return request.redirect('/marques')
+
+        domain = [
+            ('pool_brand_id', '=', brand_id),
+            ('website_id', '=', request.website.id),
+            ('is_published', '=', True),
+        ]
+
+        Product = request.env['product.template'].sudo()
+        products = Product.search(domain, limit=20, offset=page * 20)
+        product_count = Product.search_count(domain)
+
+        pager = request.website.pager(
+            url=f'/shop/marque/{brand_id}',
+            total=product_count,
+            page=page,
+            step=20,
+        )
+
+        return request.render('lolirine_pool_website.shop_brand_page', {
+            'brand': brand,
+            'products': products,
+            'product_count': product_count,
+            'pager': pager,
         })
