87e0699bb14fa0f66dc53d89c038f7ff69d2908c
Lolirine <gardemeublelolirine@gmail.com>
2026-04-04 18:29:55 +0200

    Delete lolirine_storage_notify directory

--- lolirine_storage_notify/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import notify_mixin
-from . import push_subscription
-from . import res_users
-from . import calendar_event
-from . import crm_lead
-from . import mail_message
-from . import res_config_settings
