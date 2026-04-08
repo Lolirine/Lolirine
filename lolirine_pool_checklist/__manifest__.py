37030c77328c59d633b510ff8f225ed8de24dd5a
Lolirine <gardemeublelolirine@gmail.com>
2026-04-08 18:10:37 +0200

    Add files via upload

---
+++ lolirine_pool_checklist/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Lolirine — Fiche de visite piscine',
+    'version': '19.0.2.0.0',
+    'summary': 'Fiche de visite chantier piscine — persistance, photos, signature, devis',
+    'author': 'Lolirine SRL',
+    'website': 'https://www.lolirinepoolstore.be',
+    'category': 'Website',
+    'license': 'LGPL-3',
+    'depends': ['website', 'website_sale', 'product', 'sale', 'contacts'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/report_model_views.xml',
+        'views/templates.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
