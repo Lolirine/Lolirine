3d1f871f669c868df3004dcb87e989e47d2dd3a5
Lolirine <gardemeublelolirine@gmail.com>
2026-05-02 18:13:32 +0200

    Add box_consistency_wizard import to __init__.py

--- lolirine_invoice/wizard/__init__.py
+++ lolirine_invoice/wizard/__init__.py
@@ -3,3 +3,4 @@ from . import invoice_mass_send_wizard
 from . import invoice_export_wizard
 from . import lolirine_refund_wizard
 from . import invoice_audit_wizard
+from . import box_consistency_wizard
