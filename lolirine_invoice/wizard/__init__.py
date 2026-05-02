eaabb889c9e5a7955fef3af9a95702eae164ba3f
Lolirine <gardemeublelolirine@gmail.com>
2026-05-02 17:18:42 +0200

    Add invoice_audit_wizard import to __init__.py

--- lolirine_invoice/wizard/__init__.py
+++ lolirine_invoice/wizard/__init__.py
@@ -2,3 +2,4 @@ from . import invoice_send_wizard
 from . import invoice_mass_send_wizard
 from . import invoice_export_wizard
 from . import lolirine_refund_wizard
+from . import invoice_audit_wizard
