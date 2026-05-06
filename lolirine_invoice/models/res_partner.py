c3ece0366a031549b69a2375a05a1e88d784482a
Lolirine <gardemeublelolirine@gmail.com>
2026-05-06 18:25:08 +0200

    Add manual payment field to ResPartner model
    Add a boolean field for manual payment option in ResPartner model.

--- lolirine_invoice/models/res_partner.py
+++ lolirine_invoice/models/res_partner.py
@@ -1,8 +1,17 @@
-from odoo import models
+from odoo import models, fields
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    lolirine_manual_payment = fields.Boolean(
+        string="Paiement manuel",
+        default=False,
+        help="Si coché : les factures de ce fournisseur ne sont pas en domiciliation "
+             "automatique et doivent être payées manuellement (virement à effectuer). "
+             "Une alerte orange apparaît dans la liste des factures fournisseurs "
+             "tant que la facture n'est pas payée.",
+    )
 
     def _get_followup_attachments(self, options):
         """
@@ -12,11 +21,11 @@ class ResPartner(models.Model):
         self.ensure_one()
         res_attachment_ids = []
         followup_line = options.get('followup_line')
-        
+
         # NE PAS ajouter le rapport de relance Odoo
         # options['report_attachment_id'] = self._get_followup_report(options)
         # res_attachment_ids.append(options['report_attachment_id'])
-        
+
         # Ajouter les attachments du template email
         if template_id := options.get('template_id', followup_line.mail_template_id):
             template_attachments = template_id._generate_template_attachments(
@@ -32,17 +41,17 @@ class ResPartner(models.Model):
                     'res_id': self.id,
                 })
             res_attachment_ids += self.env['ir.attachment'].create(attachments_to_create).ids
-        
+
         # Vérifier si on doit joindre les factures
         if not options.get('join_invoices', followup_line.join_invoices):
             return res_attachment_ids
-        
+
         if options.get('manual_followup'):
             # Pour les relances manuelles, utiliser les attachments sélectionnés
             res_attachment_ids += options.get('attachment_ids', [])
             return res_attachment_ids
-        
+
         # Ajouter les PDFs des factures en retard
         res_attachment_ids += self._get_invoices_to_print(options).message_main_attachment_id.ids
-        
+
         return res_attachment_ids
