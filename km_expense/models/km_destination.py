dabd03bcd148fc99f3fbbf222d930e94b6b33519
Lolirine <gardemeublelolirine@gmail.com>
2026-07-09 16:35:26 +0200

    Rename unique_destination_depart to _unique_destination_depart

--- km_expense/models/km_destination.py
+++ km_expense/models/km_destination.py
@@ -153,7 +153,7 @@ class KmDestinationDistance(models.Model):
         for record in self:
             record.distance_aller_retour = record.distance_km * 2
 
-    unique_destination_depart = models.Constraint(
+    _unique_destination_depart = models.Constraint(
         'UNIQUE(destination_id, lieu_depart_id)',
         "Une distance existe déjà pour ce couple lieu de départ / destination!",
     )
